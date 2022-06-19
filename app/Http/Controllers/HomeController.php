a1359f13fb079e32e418495a6e58503c5a706550
kelkel17 <saturre.mic2@gmail.com>
2022-06-19 21:39:42 +0800

    Added display all trades feature

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use App\Models\Trade;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $trades = Trade::whereUserId(Auth::user()->id)->get();
+
+        return view('home', compact('trades'));
     }
 }
--- resources/views/components/trade/index.blade.php
+++ resources/views/components/trade/index.blade.php
@@ -27,6 +27,7 @@
                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder text-center opacity-7 ps-2">Profit</th>
                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder text-center opacity-7 ps-2">New Balance</th>
                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder text-center opacity-7 ps-2">Additional/Deduction</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder text-center opacity-7 ps-2">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -82,40 +83,68 @@
                         <td class="align-middle text-center">
                             <span class="text-xs font-weight-bold">No comment</span>
                         </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">Edit</span>
+                        </td>
                     </tr>
-                    {{-- <tr>
-                        <td>
-                            <div class="d-flex px-2">
-                                <div>
-                                    <img src="{{ asset('img/small-logos/logo-xd.svg') }}" class="avatar avatar-sm rounded-circle me-2">
-                                </div>
-                                <div class="my-auto">
-                                    <h6 class="mb-0 text-sm">Adobe XD</h6>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <p class="text-sm font-weight-bold mb-0">$2,300</p>
-                        </td>
-                        <td>
-                            <span class="text-xs font-weight-bold">done</span>
+                    @foreach ($trades as $trade)
+                    <tr>
+                        <td class="align-middle text-center">
+                            <p class="text-sm font-weight-bold mb-0">{{ $trade->trade_date }}</p>
                         </td>
                         <td class="align-middle text-center">
-                            <div class="d-flex align-items-center justify-content-center">
-                                <span class="me-2 text-xs font-weight-bold">100%</span>
-                                <div>
-                                    <div class="progress">
-                                        <div class="progress-bar bg-gradient-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
-                                    </div>
-                                </div>
-                            </div>
+                            <p class="text-sm font-weight-bold mb-0">{{ $trade->lot_size }}</p>
                         </td>
-                        <td class="align-middle">
-                            <button class="btn btn-link text-secondary mb-0" aria-haspopup="true" aria-expanded="false">
-                                <i class="fa fa-ellipsis-v text-xs" aria-hidden="true"></i>
-                            </button>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">3.6</span>
                         </td>
-                    </tr> --}}
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">1.1234567</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">1.1234567</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">1.1234567</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">56</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">$25</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">AUDNZD</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">Sell</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">22:15</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">22:20</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">4.2</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">9.5</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">5.3</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">30.3</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">No comment</span>
+                        </td>
+                        <td class="align-middle text-center">
+                            <span class="text-xs font-weight-bold">Edit</span>
+                        </td>
+                    </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
